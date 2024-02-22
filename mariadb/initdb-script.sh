8237eae77541726ab928b74cfb7f7905b0dab485
Ewen35550 <ewen.herve.35550@gmail.com>
2024-02-22 12:32:42 +0100

    config nginx et load-balancing

--- mariadb/initdb-script.sh
+++ mariadb/initdb-script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
+# Exécuter le script SQL pour créer les utilisateurs
 mysql < ./create-users.sql
 
 echo "Running custom initialization scripts..."
 
-# Exécuter le script SQL pour créer la table
+# Exécuter le script SQL pour créer la base de données
 mysql < ./create-database.sql
