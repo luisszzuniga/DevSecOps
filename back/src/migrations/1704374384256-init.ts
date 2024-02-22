0d4778975dd99552f52d5d1aec97e7871e1c5a66
Luis Zuniga <luiszu7779@gmail.com>
2024-02-22 11:41:42 +0100

    Encrypt DB

--- back/src/migrations/1704374384256-init.ts
+++ back/src/migrations/1704374384256-init.ts
@@ -7,10 +7,6 @@ export class Init1704374384256 implements MigrationInterface {
     await queryRunner.query(
       `CREATE TABLE \`users\` (\`id\` int NOT NULL AUTO_INCREMENT, \`firstname\` varchar(50) NOT NULL, \`lastname\` varchar(50) NOT NULL, \`birthdate\` date NOT NULL, \`email\` varchar(255) NOT NULL, \`phone\` varchar(10) NOT NULL, \`address\` text NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
     );
-
-    await queryRunner.query(
-      `ALTER TABLE users ENCRYPTED=YES ENCRYPTION_KEY_ID=1`
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
